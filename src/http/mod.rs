cf3551a6b58256a79f72d29f19b238f23702b5db
Akash Pathak <pathakvikash9211@gmail.com>
2021-09-23 02:08:46 +0530

    Complete http server with basic function

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,5 +1,12 @@
 pub use method::Method;
 pub use request::Request;
+pub use request::ParseError;
+pub use query_string::QueryString;
+pub use status_code::StatusCode;
+pub use response::Response;
 
 pub mod method;
 pub mod request;
+pub mod query_string;
+pub mod status_code;
+pub mod response;
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,17 @@
-use std::net::TcpListener;
+use crate::http::{ParseError, Request, Response, StatusCode};
+use std::convert::TryFrom;
 use std::io::Read;
+use std::net::TcpListener;
+
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to parse request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+}
 
 pub struct Server {
     addr: String
@@ -11,7 +23,7 @@ impl Server {
           addr
         }
     }
-    pub fn run(&self) {
+    pub fn run(&self, mut handler: impl Handler) {
         println!("Server is running on {}",self.addr);
         let listener = TcpListener::bind(&self.addr).unwrap();
 
@@ -22,7 +34,17 @@ impl Server {
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
                         Ok(_) => {
-                            println!("{}",String::from_utf8_lossy(&buffer))
+                            // println!("{}",String::from_utf8_lossy(&buffer));
+                           
+                            let response = match Request::try_from(&buffer[..]) {
+                                Ok(request) => handler.handle_request(&request),
+                                Err(e) => handler.handle_bad_request(&e),
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
+                            }
+
                         }
                         Err(e) => {
                             println!("failed to read stream Error {}", e)
